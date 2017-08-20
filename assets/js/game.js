d583870f23f20e6d8ae68936df195c9965373faa
plustim <tim@plustim.com>
2017-08-19 21:21:53 -0700

    more progress

--- assets/js/game.js
+++ assets/js/game.js
@@ -37,7 +37,7 @@ $(document).ready(function(){
 			charactersLeft.push(index);
 			playerOptions += buildCharacter(index);
 		});
-		$("#player-options").html(playerOptions);
+		$("#player").html(playerOptions);
 		$("#opponents").html("");
 		$("#battle").html("");
 	}
@@ -46,38 +46,61 @@ $(document).ready(function(){
 		var card = "<div class='option' data-number='" + id + "'>
 				<div class='portrait' style='background-image:url(" + characters[id].portrait + ");'>
 				<h3>" + characters[id].name + "</h3>
-				<div class='hp'></div>" + characters[id].hp + "
+				<div class='hp-holder'>
+					<div class='hp'></div>
+				</div>" + characters[id].hp + "
 			</div>";
+		{}// remove this to make it work. don't know why sublime stops syntax highlighting after a line break in quotes.
 		return card;
 	}
 
 	function attack(){
-		// attack animation
 		// attack calculation 
+		if( opponent.hp > player.attack ){
+			opponent.hp -= player.attack;
+		}else{
+			opponent.hp = 0;
+		}
 		// increase attack stat
+		player.attack += 1;
 		// hp animation
+		var hpBar = opponent.hp / character[opponent.id].hp * 100 + "%";
+		$("#battle .hp").css("width", hpBar);
 		// win check (return bool for result)
+		return ( opponent.hp === 0 );
 	}
 
 	function defend(){
 		// counter attack animation
 		// counter attack calculation
+		if( player.hp > opponent.attack ){
+			player.hp -= opponent.attack;
+		}else{
+			player.hp = 0;
+		}
 		// hp animation
+		var hpBar = player.hp / character[player.id].hp * 100 + "%";
+		$("#player .hp").css("width", hpBar);
 		// lose check (return bool for result)
 	}
 
 	// user clicks character
-	$("#player-options").on("click", ".option", function(){
+	$("#player").on("click", ".option", function(){
 		// set player character
 		player = characters[$(this).data("number")]; //todo: to give these elements data-number attr
 		// show player character splash
 		// populate options for opponents
 		charactersLeft.splice( $(this).data("number"), 1 );
 		// show opponents box
+		var displayOpponents = "";
+		$.each(charactersLeft, function(index, value){
+			displayOpponents += buildCharacter( value );
+		});
+		$("#opponents").html(displayOpponents);
 	});
 
 	// user clicks opponent
-	$("#opponents").on("click", "option", function(){
+	$("#opponents").on("click", ".option", function(){
 		// disable opponents box
 		// set current opponent
 		opponent = characters[$(this).data("number")];
@@ -86,7 +109,7 @@ $(document).ready(function(){
 		// re-populate options for opponents
 		var displayOpponents = "";
 		$.each(charactersLeft, function(index, value){
-			displayOpponents += "<div class='option' data-number='" + value + "'><div class='portrait' style='background-image:url(" + characters[index].portrait + ");'><h3>" + characters[index].name + "</h3><div class='full-hp'></div>" + characters[index].hp + "</div>";
+			displayOpponents += buildCharacter( value );
 		});
 		$("#opponents").html(displayOpponents);
 		// show battle scene
@@ -95,27 +118,47 @@ $(document).ready(function(){
 	// user clicks attack button
 	$("#battle").on("click", "#attack", function(){
 		// disable attack button
-		// attack
-		var attackWon = attack();
-		if( attackWon ){
-			wins++;
-			// end battle scene
-			if( wins >= characters.length-1 ){
-				// win game
-			}else{
-				// win notification
-				// re-enable opponents box
-			}
-		}else{
-			// defend
-			var counterWon = defend();
-			if ( counterWon ){
-				// game over
-				// The force was not with you this time.
-				// You were fighting Space-Wizards, what did you expect?
-			}
-		}
-		// re-enable attack button
+		// attack animation
+		$("#player .option").css({"right": "100px", "transform": "rotate(-30deg)"});
+		setTimeout(function() {
+			$("#player .option").css({"right": "0px", "transform": "rotate(0deg)"});
+		}, 300);
+		// delay for attack animation
+		setTimeout(function() {
+			// attack
+			var attackWon = attack();
+			// delay for hp animation
+			setTimeout(function() {
+				if( attackWon ){
+					wins++;		
+					// end battle scene
+					if( wins >= characters.length-1 ){
+						// win game
+					}else{
+						// win notification
+						// re-enable opponents box
+					}
+				}else{
+					$("#battle .option").css({"left": "100px", "transform": "rotate(30deg)"});
+					setTimeout(function() {
+						$("#battle .option").css({"left": "0px", "transform": "rotate(0deg)"});
+					}, 300);
+					// delay for attack animation
+					setTimeout(function() {
+						// defend
+						var counterWon = defend();
+						// delay for hp animation
+						setTimeout(function() {
+							if ( counterWon ){
+								// game over
+								// The force was not with you this time.
+								// You were fighting Space-Wizards, what did you expect?
+							}
+							// re-enable attack button
+						}, 600);
+					}, 500);	
+				}
+			}, 600);
+		}, 500);	
 	});
-
 };
